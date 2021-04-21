18eb7e26e7cb6a3f4b6f4e5d71290898cf2dd351
Igor Ljevak <igorljevak@gmail.com>
2021-04-21 13:07:57 +0200

    igor profil

--- src/components/Designers.jsx
+++ src/components/Designers.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
-import {Link} from "react-router-dom";
-import JoelAvatar from '../img/Joel-Avatar.jpg'
-import MatildasAvatar from '../img/Matilda-Avatar.jpg'
-import MaxsAvatar from '../img/Max-Avatar.png'
-import IgorsAvatar from '../img/Igor-Avatar.png'
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import JoelAvatar from '../img/Joel-Avatar.jpg';
+import MatildasAvatar from '../img/Matilda-Avatar.jpg';
+import MaxsAvatar from '../img/Max-Avatar.png';
+import IgorsAvatar from '../img/Igor-Avatar.png';
 
 export default function Designers() {
-    return (
-        <div className="designOutsideContainer">
-        <h1 className="designTitle">designers</h1>
-        <div className="designContainer">
+  return (
+    <div className="designOutsideContainer">
+      <h1 className="designTitle">designers</h1>
+      <div className="designContainer">
         {/* div className="main-container-designer">
       
           <Link to="/">hem</Link>
@@ -19,45 +18,55 @@ export default function Designers() {
           <Link to="/">portfolio </Link>
        
     </div>  */}
-     <div className="profile-grid">
-
+        <div className="profile-grid">
           <div className="TextInfo">
-          <Link to="/">hem</Link>
-          <Link to="/">om oss</Link>
-          <Link to="/Designers">designers</Link>
-          <Link to="/">portfolio </Link>
+            <Link to="/">hem</Link>
+            <Link to="/">om oss</Link>
+            <Link to="/Designers">designers</Link>
+            <Link to="/">portfolio </Link>
           </div>
 
-       
-          <div className="pMatilda"><img className= "MatildasPic" src={MatildasAvatar} alt="Matilda"/><div className="TextMatilda">
-             <Link to="/Matilda">Matilda Stridsman</Link> <h4>Titel: Scrum Master</h4>  
-             <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing
-             elit. Cum incidunt ut corrupti optio ea culpa</p></div></div>
+          <div className="pMatilda">
+            <img className="MatildasPic" src={MatildasAvatar} alt="Matilda" />
+            <div className="TextMatilda">
+              <Link className="designerName" to="/Matilda">
+                Matilda Stridsman
+              </Link>{' '}
+              <h4 className="designerTitle">Titel: Scrum Master</h4>
+            </div>
+          </div>
 
-          <div className="pMax"><img className= "MaxsPic" src={MaxsAvatar} alt="Max"/> <div className="TextMax">
-          <Link to="/Max">Max Zetterlund</Link><h4>Titel: Github Master</h4>   
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing
-             elit. Cum incidunt ut corrupti optio ea culpa</p></div></div>
+          <div className="pMax">
+            <img className="MaxsPic" src={MaxsAvatar} alt="Max" />{' '}
+            <div className="TextMax">
+              <Link className="designerName" to="/Max">
+                Max Zetterlund
+              </Link>
+              <h4 className="designerTitle">Titel: Github Master</h4>
+            </div>
+          </div>
 
-          <div className="pIgor"><img className= "IgorsPic" src={IgorsAvatar} alt="Igor"/> <div className="TextIgor">
-          <Link to="/Igor">Igor Ljevak</Link><h4>Titel: Pro Designer</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing
-             elit. Cum incidunt ut corrupti optio ea culpa</p></div></div>
+          <div className="pIgor">
+            <img className="IgorsPic" src={IgorsAvatar} alt="Igor" />{' '}
+            <div className="TextIgor">
+              <Link className="designerName" to="/Igor">
+                Igor Ljevak
+              </Link>
+              <h4 className="designerTitle">Titel: Pro Designer</h4>
+            </div>
+          </div>
 
-          <div className="pJoel"><img className= "JoelsPic" src={JoelAvatar} alt="Joel"/> <div className="TextJoel">
-          <Link to="/Joel">Joel Morales </Link><h4>Titel: Pro tester</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing
-             elit. Cum incidunt ut corrupti optio ea culpa</p></div></div>
-        
-  
-
-         
+          <div className="pJoel">
+            <img className="JoelsPic" src={JoelAvatar} alt="Joel" />{' '}
+            <div className="TextJoel">
+              <Link className="designerName" to="/Joel">
+                Joel Morales{' '}
+              </Link>
+              <h4 className="designerTitle">Titel: Pro tester</h4>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-    </div> 
-    )
+  );
 }
--- src/pages/Igor.jsx
+++ src/pages/Igor.jsx
@@ -1,21 +1,33 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React from 'react';
+import AvatarIgor from '../img/Igor-Avatar.png';
+import { Link } from 'react-router-dom';
 
 export default function IgorsPage() {
-    return (
-        <div className="designOutsideContainer">
-        <h1 className="designTitle">designer</h1>
-        <div className="designContainer">
-        <h1>Igors sida</h1>
-        <div className="profile-grid">
-
-        <div className="TextInfo">
+  return (
+    <div className="main-containerIgor">
+      <div className="inner-containerIgor">
+        <div className="left-containerIgor">
+          <h1 className="studiotitleIgor">Igor</h1>
+          <h1 className="gruppTitleIgor">Ljevak</h1>
+          <img className="frontPictureIgor" src={AvatarIgor} alt="" />
+        </div>
+        <div className="mid-containerIgor">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure dicta nisi similique odio totam ad qui quod quis ratione provident quos,
+            velit eos maiores rem nam! Accusantium enim natus ipsam modi sint, quae error magnam iusto, ducimus quisquam suscipit possimus id
+            exercitationem unde nobis quibusdam neque reiciendis repudiandae aspernatur voluptatem ullam earum minus aliquid? Eveniet similique, non
+            cupiditate reiciendis quo, mollitia corporis repellendus quibusdam quam nesciunt officia eos itaque rerum optio, quos quaerat! Magni, non
+            labore! At iusto possimus iste iure accusantium fugit nihil animi quo delectus et, quibusdam itaque praesentium culpa facilis voluptate.
+            Iste fugiat doloremque excepturi qui corrupti!
+          </p>
+        </div>
+        <div className="right-containerIgor">
           <Link to="/">hem</Link>
           <Link to="/">om oss</Link>
           <Link to="/Designers">designers</Link>
-          <Link to="/">portfolio </Link>
-          </div>
-        </div> </div>
+          <Link to="/">portfoilio</Link>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
