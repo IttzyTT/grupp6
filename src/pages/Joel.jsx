7a11f7331b3e2fb99f6a809a5bdeebf4b117be75
JoelMoralesa <70750970+JoelMoralesa@users.noreply.github.com>
2021-04-21 18:41:41 +0200

    Joels profile page done

--- src/pages/Joel.jsx
+++ src/pages/Joel.jsx
@@ -1,21 +1,31 @@
 import React from 'react'
 import {Link} from "react-router-dom";
+import JoelsAvatar from '../img/Joel-Avatar.jpg';
 
 export default function JoelsPage() {
-    return (
-        <div className="designOutsideContainer">
-        <h1 className="designTitle">Tester</h1>
-        <div className="designContainer">
-        <h1>Joels sida</h1>
-        <div className="profile-grid">
+  return (
+    <div className="main-container">
+    <div className="inner-container">
+      <div className="left-container">
+        <h1 className="Joel-designTitle">Joel</h1>
+        <h1 className="Joel-Title">Morales</h1>
+        <img className="JoelsPic" src={JoelsAvatar} alt="" />
 
-<div className="TextInfo">
-  <Link to="/">hem</Link>
-  <Link to="/">om oss</Link>
-  <Link to="/Designers">designers</Link>
-  <Link to="/">portfolio </Link>
+      </div>
+      <div className="mid-container-Joel">
+        <p>Joel - Tester <br></br><br></br>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem, quis similique mollitia obcaecati veniam dolores
+          <br></br>hic placeat maiores ipsa eveniet architecto eius, accusantium consectetur fugit exercitationem quaerat a animi perferendis?
+          hic placeat maiores ipsa eveniet architecto eius, accusantium consectetur fugit exercitationem quaerat a animi perferendis?
+          hic placeat maiores ipsa eveniet architecto eius, accusantium consectetur fugit exercitationem quaerat a animi perferendis?</p>
+
+      </div>
+      <div className="right-container">
+        <Link to="/">hem</Link>
+        <Link to="/">om oss</Link>
+        <Link to="/Designers">designers</Link>
+        <Link to="/">portfoilio</Link>
+      </div>
+    </div>
   </div>
-</div> </div>
-</div>
-    )
+  )
 }
